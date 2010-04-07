a0ce014ed1972d9c6aeea12a883ca9d80345583d
cberzan <cberzan@cc622a4f-0c80-7b79-afa9-a082597f7fa1>
2010-04-07 16:22:20 +0000

    * add barebones level zero and terminal entity (doesn't do anything yet) * refactor CollisionManager::getEntityTiles() a little to prevent crashes and better see what's going on.
    I don't like the player sinking into the walls :-/ I'll try to change collision detection to avoid that.

---
+++ flock/src/flock/TerminalEntity.java
@@ -0,0 +1,14 @@
+package flock;
+
+public class TerminalEntity extends Entity
+{
+	public TerminalEntity(double x, double y)
+	{
+		super("terminal", x, y);
+	}
+
+	@Override
+	public void doUpdate(long msElapsed)
+	{
+	}
+}
