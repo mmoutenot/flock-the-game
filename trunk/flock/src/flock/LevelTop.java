cfb44bdb138639c1fd781d8b517a6c707ee06547
cberzan <cberzan@cc622a4f-0c80-7b79-afa9-a082597f7fa1>
2010-04-11 02:03:39 +0000

    Making the FactoryEntity spit Lemmings both left and right broke LevelTop. Fixed it.

--- trunk/flock/src/flock/LevelTop.java
+++ trunk/flock/src/flock/LevelTop.java
@@ -7,7 +7,9 @@ public class LevelTop extends Level
 		super("top");
 		_entities.add(new DoorEntity(600, 165, 10, "zero"));
 		_entities.add(new WrenchEntity(100, 300));
-		_entities.add(new FactoryEntity(150, 30, false, 15, 1000));
+		FactoryEntity factory = new FactoryEntity(150, 30, false, 15, 1000);
+		factory.setBothWays(false);
+		_entities.add(factory);
 		_entities.add(new PlayerEntity(100, 470));
 	}
 }
