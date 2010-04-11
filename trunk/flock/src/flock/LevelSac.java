a1d25f5d5817e6dce26d1ebcb0e6f8f935eacbcc
TennaTrista <TennaTrista@cc622a4f-0c80-7b79-afa9-a082597f7fa1>
2010-04-11 23:19:02 +0000

    Added a level where the player has to drop a block down somewhere to keep the anti-lemmings away. Also, made the wrench a little smaller since it was kind of too big. Feel free to change it back.

---
+++ trunk/flock/src/flock/LevelSac.java
@@ -0,0 +1,20 @@
+package flock;
+
+public class LevelSac extends Level
+{
+	public LevelSac() throws Exception
+	{
+		super("sac");
+		
+		_entities.add(new WrenchEntity(300, 200));
+		_entities.add(new DoorEntity(40, 524, 2, "zero"));
+		_entities.add(new FactoryEntity(100, 0, true, 5, 1000));
+		FactoryEntity f = new FactoryEntity(150, 150, false, 5, 1000);
+		f.setBothWays(false);
+		_entities.add(f);
+		_entities.add(new FactoryEntity(50, 400, true, 5, 1000));
+		_entities.add(new BlockEntity(250, 190));
+		
+		_entities.add(new PlayerEntity(300, 150));
+	}
+}
