98458354e5fab775ef58f700c5beb9164078a430
cberzan <cberzan@cc622a4f-0c80-7b79-afa9-a082597f7fa1>
2010-04-11 22:25:32 +0000

    Add a simple new level that requires very careful timing.

---
+++ trunk/flock/src/flock/LevelButtons.java
@@ -0,0 +1,47 @@
+package flock;
+
+public class LevelButtons extends Level
+{
+	private class CustomButton extends ButtonEntity
+	{
+		public CustomButton(double x, double y)
+		{
+			super(x, y);
+		}
+
+		@Override
+		protected void stateChanged(boolean state)
+		{
+			final int[][] touch =
+			{
+					{  9, 12 },
+					{ 10, 11 },
+					{ 10, 12 },
+					{ 10, 13 },
+					{ 13, 11 },
+					{ 13, 12 },
+					{ 13, 13 }
+			};
+			Tile[][] tiles = Game.instance().getTiles();
+			
+			for(int i = 0; i < touch.length; i++)
+			{
+				Tile t = state ? new WallTile() : new AirTile();
+				tiles[ touch[i][0] ][ touch[i][1] ] = t;
+			}
+		}
+	}
+	
+	public LevelButtons() throws Exception
+	{
+		super("buttons");
+		
+		_entities.add(new DoorEntity(700, 520, 2, "zero"));
+		_entities.add(new FactoryEntity(100, 100, false, 2, 1500));
+		_entities.add(new FactoryEntity(530, 100, true, 2, 1000));
+		_entities.add(new CustomButton(300, 500));
+		
+		_entities.add(new PlayerEntity(340, 100));
+	}
+}
+
